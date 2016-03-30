eba4b6fb9b3ec7b975279c589800713b5aba4bb4
Benjamin Borbe <bborbe@rocketnews.de>
2016-03-30 23:37:54 +0200

    add ssl script

---
+++ scripts/generate-self-signed-cert.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout ssl/benjamin-borbe.key -out ssl/benjamin-borbe.crt
+cat ssl/benjamin-borbe.crt ssl/benjamin-borbe.key > ssl/benjamin-borbe.pem
