4b2fe39bbe15a4f257e2a5c10bc3b381a9e13f76
Benjamin Borbe <bborbe@rocketnews.de>
2016-04-11 23:10:51 +0200

    use k8s for harteslicht portfolio

--- generate-hm-config.sh
+++ generate-hm-config.sh
@@ -11,11 +11,11 @@ K8S_ENDPOINT_0="172.16.20.20"
 K8S_ENDPOINT_1="172.16.20.21"
 K8S_ENDPOINT_2="172.16.20.22"
 
-BB_EXTERN="ipfire.hm.benjamin-borbe.de"
-BB_INTERN="ipfire.hm.benjamin-borbe.de"
+BB_EXTERN="bb.ipfire.hm.benjamin-borbe.de"
+BB_INTERN="bb.ipfire.hm.benjamin-borbe.de"
 
-HL_EXTERN="hm.harteslicht.com"
-HL_INTERN="hm.harteslicht.com"
+HL_EXTERN="hl.ipfire.hm.benjamin-borbe.de"
+HL_INTERN="hl.ipfire.hm.benjamin-borbe.de"
 
 ################################################################################
 
