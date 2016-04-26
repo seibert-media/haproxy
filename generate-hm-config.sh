15761002177e6b889cfb9aabbc161aca44261ff2
Benjamin Borbe <bborbe@rocketnews.de>
2016-04-26 22:24:56 +0200

    remove intern

--- generate-hm-config.sh
+++ generate-hm-config.sh
@@ -6,16 +6,12 @@ set -o pipefail
 
 ################################################################################
 
-MISC="172.16.60.23"
 K8S_ENDPOINT_0="172.16.20.20"
 K8S_ENDPOINT_1="172.16.20.21"
 K8S_ENDPOINT_2="172.16.20.22"
 
 BB_EXTERN="bb.ipfire.hm.benjamin-borbe.de"
-BB_INTERN="bb.ipfire.hm.benjamin-borbe.de"
-
 HL_EXTERN="hl.ipfire.hm.benjamin-borbe.de"
-HL_INTERN="hl.ipfire.hm.benjamin-borbe.de"
 
 ################################################################################
 
--- generate-rn-config.sh
+++ generate-rn-config.sh
@@ -6,16 +6,12 @@ set -o pipefail
 
 ################################################################################
 
-MISC="172.16.50.23"
 K8S_ENDPOINT_0="172.16.30.20"
 K8S_ENDPOINT_1="172.16.30.21"
 K8S_ENDPOINT_2="172.16.30.22"
 
 BB_EXTERN="benjamin-borbe.de"
-BB_INTERN="rn.benjamin-borbe.de"
-
 HL_EXTERN="harteslicht.com"
-HL_INTERN="rn.harteslicht.com"
 
 ################################################################################
 
