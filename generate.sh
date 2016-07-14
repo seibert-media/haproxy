dabe9a05d913055a95c8ee057bc56a2544dfaac1
Benjamin Borbe <bborbe@rocketnews.de>
2016-07-14 13:41:16 +0200

    split haproxy repo

--- generate.sh
+++ generate.sh
@@ -6,8 +6,6 @@ set -o pipefail
 
 ################################################################################
 
-HAPROXY_TEMPLATE="template/haproxy-sm.cfg"
-
 K8S_ENDPOINT_0="172.16.11.15"
 K8S_ENDPOINT_1="172.16.11.16"
 K8S_ENDPOINT_2="172.16.11.17"
